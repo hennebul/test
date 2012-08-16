31f196badbf8fa3f8b84d0355e56de2ab82e8745
Liam Hennebury <liamhennebury@gmail.com>
2012-08-15 23:24:47 -0300

    Still learning

--- test.js
+++ test.js
@@ -1,44 +1,103 @@
-// Create our web server.
-var http = require('http').createServer(handler);
+//*****************************************************************************
 
-// Create our chat server.
+//-----------------------------------------------------------------------------
+// :: Variables
+
+// Web server.
+var http = require('http').createServer(handleReq);
+
+// Chat server.
 var server = require('socket.io').listen(http);
 
-// Create a file system object.
+// File system object.
 var fs = require('fs');
 
-// Serve up our one and only page.
-function handler(req, res) {
-	fs.readFile(__dirname + '/test.html', function(err, data) {
-		// Make sure the page was read.
-		if (err) {
-			res.writeHead(500);
-			return res.end('Error loading test.html');
-		}
+// The page to return for all http requests.
+var page;
 
-		// Send back the page.
-		res.writeHead(200);
-		res.end(data);
-    });
+//-----------------------------------------------------------------------------
+// :: Functions
+
+/**
+ * Handle all http requests by returning our one and only page.
+ */
+function handleReq(req, res) {
+    // Load the page from the file system, if needed.
+    if (typeof page === 'undefined') {
+        fs.readFile(__dirname + '/test.html', function(err, data) {
+            // Make sure the page was read.
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading test.html');
+            }
+            
+            // Save the page contents so they aren't loaded for each request.
+            page = data;
+    
+            // Send back the page.
+            res.writeHead(200);
+            res.end(page);
+        });
+    } else {
+        // Send back the page.
+        res.writeHead(200);
+        res.end(page);
+    }
 }
 
-// Make the web server listen on a different port.
+/**
+ * Handle a message from the client.
+ */
+function handleMessage(message, client) {
+    switch(message.type) {
+    // Create a chat room.
+    case 'create':
+        break;
+    // Join a chat room.
+    case 'join':
+        client.join(message.room);
+        
+        // Tell the other users that the user has joined.
+        client.broadcast.to(message.room)
+            .emit('message', message.user + ' has joined');
+        break;
+    // Leave a chat room.
+    case 'leave':
+        // Tell the other users that the user has left, then leave.
+        client.broadcast.to(message.room)
+            .emit('message', message.user + ' has left');
+        //client.leave(message.room);
+        break;
+    // Chat with users in a chat room.
+    case 'chat':
+        client.broadcast.to(message.room)
+            .emit('message', message.user + ': ' + message.text);
+        break;
+    default:
+        console.log('Received unrecognized message: '
+                    + JSON.stringify(message));
+    }
+}
+
+//-----------------------------------------------------------------------------
+// :: Main
+
+// Start the web server.
 http.listen(8080);
 
-// For each connection join them to the chat.
-server.sockets.on('connection', function(socket) {
-	socket.join('chat');
-	
-	// Tell the other users that a new user has joined.
-	socket.broadcast.to('chat').emit('message', 'new user joined');
-	
-	// Broadcast each message to all of the other users in the chat.
-	socket.on('message', function(data) {
-		socket.broadcast.to('chat').emit('message', data);
+// Handle incoming client connections.
+server.sockets.on('connection', function(client) {
+    // TODO: Send back the list of chat rooms.
+    
+	// Dispatch all received messages to handleMessage().
+	client.on('message', function(message) {
+		handleMessage(message, client);
 	});
 	
 	// Tell the other users that a user has left.
-	socket.on('disconnect', function() {
-		socket.broadcast.to('chat').emit('message', 'user has left the chat');
+	client.on('disconnect', function() {
+		client.broadcast.to('chat').emit('message', 'user has left the chat');
 	});
 });
+
+//*****************************************************************************
